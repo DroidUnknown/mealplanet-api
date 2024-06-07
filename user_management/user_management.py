6d6f83ecb417d4c458d3c9dc62fe7424797cbdc4
Haseeb1424 <57188404+Haseeb1424@users.noreply.github.com>
2024-06-07 22:39:24 +0500

    password encryption added

--- user_management/user_management.py
+++ user_management/user_management.py
@@ -185,20 +185,24 @@ def verify_user_otp(user_id):
 
     db_engine = jqutils.get_db_engine()
     
-    # encrypt password
-    query = text(""" 
-            select symmetric_key 
-            from portal_profile_service_secret 
-            where key_name = 'password-protector-key' and
-            meta_status = :meta_status
-        """)
-    with db_engine.connect() as conn:
-        result = conn.execute(query, meta_status='active').fetchone()
-        assert result, "no valid symmetric key found for password protector"
-        key_string_db = result['symmetric_key']
-        key_string_db_bytes = key_string_db.encode()
+    # # encrypt password
+    # query = text(""" 
+    #         select symmetric_key 
+    #         from portal_profile_service_secret 
+    #         where key_name = 'password-protector-key' and
+    #         meta_status = :meta_status
+    #     """)
+    # with db_engine.connect() as conn:
+    #     result = conn.execute(query, meta_status='active').fetchone()
+    #     assert result, "no valid symmetric key found for password protector"
+    #     key_string_db = result['symmetric_key']
+    #     key_string_db_bytes = key_string_db.encode()
 
-    cipher_text_bytes = jqsecurity.encrypt_bytes_symmetric_to_bytes(password_bytes, key_string_db_bytes)
+    # cipher_text_bytes = jqsecurity.encrypt_bytes_symmetric_to_bytes(password_bytes, key_string_db_bytes)
+
+    # encrypt password and update user
+    password_manager = DataMigrationManager()
+    encrypted_password = password_manager.encrypt_password(password)
 
     db_engine = jqutils.get_db_engine()
     
@@ -258,7 +262,7 @@ def verify_user_otp(user_id):
                         WHERE user_id = :user_id
                     """)
                     with db_engine.connect() as conn:
-                        conn.execute(query, keycloak_user_id=keycloak_user_id, username=username, password=cipher_text_bytes, user_id=user_id)
+                        conn.execute(query, keycloak_user_id=keycloak_user_id, username=username, password=encrypted_password, user_id=user_id)
 
                     response_body = {
                         "data": {
